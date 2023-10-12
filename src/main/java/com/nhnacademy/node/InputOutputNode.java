c341df0f6d98171d676191319e53ad89d3725928
jongsik22 <xxx_@kakao.com>
2023-10-12 16:36:23 +0900

    wip

--- src/main/java/com/nhnacademy/node/InputOutputNode.java
+++ src/main/java/com/nhnacademy/node/InputOutputNode.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.node;
 
 import com.nhnacademy.Wire;
-import com.nhnacademy.server.Request;
 
 public class InputOutputNode extends ActiveNode {
     Wire[] inputWires;
@@ -32,7 +31,7 @@ public class InputOutputNode extends ActiveNode {
 
     public void output(Request request) {
         for (Wire wire : outputWires) {
-            // wire.put(request);
+            wire.put(request);
         }
     }
 }
