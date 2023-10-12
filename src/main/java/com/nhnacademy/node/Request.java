1a1d325c3cc910881ae1590371e3789475baea14
SeinJ <drasein@gmail.com>
2023-10-12 20:59:25 +0900

    message 완료

--- src/main/java/com/nhnacademy/node/Request.java
+++ src/main/java/com/nhnacademy/node/Request.java
@@ -1,19 +1,16 @@
 package com.nhnacademy.node;
 
-import java.net.Socket;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.UUID;
 
 public class Request {
     private String url;
-    private Socket socket;
+    
     private UUID id;
 
-    public Request(UUID id, String url, Socket socket) {
+    public Request(UUID id, String url) {
         this.id = id;
         this.url = url;
-        this.socket = socket;
+        
     }
 
     public String getUrl() {
