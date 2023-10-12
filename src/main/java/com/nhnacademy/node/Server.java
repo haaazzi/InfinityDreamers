8cea81ff3bf6304d05c40f0462d8c8e1199c5802
jongsik22 <xxx_@kakao.com>
2023-10-12 14:33:22 +0900

    j

---
+++ src/main/java/com/nhnacademy/node/Server.java
@@ -0,0 +1,112 @@
+package com.nhnacademy.node;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+
+public class Server extends InputOutputNode {
+    class Handler implements Runnable {
+        UUID id;
+        Socket socket;
+        BufferedReader reader;
+        BufferedWriter writer;
+        Server server;
+        Thread thread;
+
+        public Handler(Socket socket, Server server) throws IOException {
+            id = UuidCreator.getTimeBased();
+            this.socket = socket;
+            this.server = server;
+            thread = new Thread(this);
+        }
+
+        public UUID getId() {
+            return id;
+        }
+
+        public void start() {
+            thread.start();
+        }
+
+        public void write(String message) throws IOException {
+            writer.write(message);
+            writer.flush();
+        }
+
+        @Override
+        public void run() {
+            try {
+                writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                }
+                server.output(new Request(builder.toString()));
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+
+    }
+
+    int port = 80;
+    ServerSocket serverSocket;
+    String id;
+    Thread receiverThread;
+    Map<UUID, Handler> handlerMap;
+
+    public Server(String name) {
+        super(name, 1, 1);
+        handlerMap = new HashMap<>();
+    }
+
+    @Override
+    void preprocess() {
+        try {
+            serverSocket = new ServerSocket(port);
+            receiverThread = new Thread(() -> {
+
+            });
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    void process() {
+        try {
+            Socket socket = serverSocket.accept();
+            System.out.println(socket.getInetAddress());
+            Handler handler = new Handler(socket, serverSocket);
+            // handler
+            handler.start();
+            handlerMap.put(handler.getId(), handler);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server("TCPServer");
+        server.start();
+    }
+}
