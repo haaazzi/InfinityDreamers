feee0066f023d3eec7829105f08005176d1d9b41
haaazzi <haa.zzing@gmail.com>
2023-10-13 15:50:20 +0900

    ADD: EMS 연결 노드

--- src/main/java/com/nhnacademy/node/URLParserNode.java
+++ src/main/java/com/nhnacademy/node/URLParserNode.java
@@ -1,6 +1,10 @@
 package com.nhnacademy.node;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 import com.nhnacademy.WireType;
 
@@ -8,9 +12,37 @@ public class URLParserNode extends InputOutputNode {
     Message message;
     String deviceId;
     HashMap<String, String> options;
+    WireType type;
+    StringBuilder requestBuilder;
 
     public URLParserNode(String name) {
         super(name, 1, 2);
+    }
+
+    public long dateConverter(String value) {
+
+        if (value.length() == 16) {
+            value += ":00";
+        } else if (value.length() == 13) {
+            value += ":00:00";
+        } else if (value.length() == 9) {
+            value += " 00:00:00";
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Date date;
+        long millis = 0;
+
+        try {
+            date = sdf.parse(value);
+            millis = date.getTime() / 1000;
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return millis;
+
     }
 
     public void process() {
@@ -18,41 +50,62 @@ public class URLParserNode extends InputOutputNode {
 
             message = getInputWire(WireType.PARSER).get();
             Request request = message.getRequest();
+
             String url = request.getUrl();
-            System.out.println(url);
-            String[] urlOptions = url.split("/");
+            String[] urlOptions = url.split("http://");
+            urlOptions = urlOptions[1].split("/");
+            urlOptions = urlOptions[1].split("\\?");
+            System.out.println(urlOptions[1]);
 
             options = new HashMap<String, String>();
 
+            // GET http://localhost/humidity?format=json&startDt=2023-10-05
+            // 15:00&endDt=2023-10-05 18:00:00&unit=hour
+            // http://ems.nhnacademy.com:1880/ep/temperature/24e124126c457594?count=40&st=1696772438&et=1696772438
             if (urlOptions[1].equals("dev")) {
 
                 if (urlOptions.length > 1) {
                     options.put("deviceId", urlOptions[2]);
                 }
 
-            } else if (urlOptions[1].equals("ep")) {
+                type = WireType.DEVICE;
+
+            } else if (urlOptions[0].equals("temperature") || urlOptions[0].equals("humidity")) {
+
+                options.put("type", urlOptions[0]);
+
+                if (urlOptions[0].equals("temperature")) {
+                    type = WireType.TEMPERATURE;
+                } else {
+                    type = WireType.HUMIDITY;
+                }
 
                 if (urlOptions.length > 1) {
 
-                    if (urlOptions[2].contains("?")) {
-                        options.put("type", urlOptions[2].split("\\?")[0]);
+                    String urlString = urlOptions[1];
 
-                        String urlString = urlOptions[2].split("\\?")[1];
+                    for (String string : urlString.split("&")) {
+                        String key = string.split("=")[0];
+                        String value = string.split("=")[1];
 
-                        for (String string : urlString.split("&")) {
-                            options.put(string.split("=")[0], string.split("=")[1]);
+                        if (key.equals("startDt") || key.equals("endDt")) {
+                            value = dateConverter(value) + "";
                         }
 
-                        // for (Map.Entry<String, String> set : options.entrySet()) {
-                        // System.out.println(set.getKey() + " = " + set.getValue());
-                        // }
-                    } else {
-                        options.put("type", urlOptions[2]);
+                        options.put(key, value);
+
                     }
+
+                    for (Map.Entry<String, String> set : options.entrySet()) {
+                        System.out.println(set.getKey() + " = " + set.getValue());
+                    }
+
                 }
             }
+
             request.setOptions(options);
-            output(message, WireType.PARSER);
+
+            output(message, type);
         }
     }
 }
